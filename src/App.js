95416d70fd0e0b67e10283f486193a1337bb0f86
Jonathan Correa Paiva - BQN <jonathan.correa@bqn.com.uy>
2022-11-05 00:18:54 -0300

    update: Separed incident_list component, handle add new test incident and delete

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
+import IncidentsList from './components/incident_list';
 
 
 function App() {
@@ -8,10 +9,35 @@ function App() {
     return Math.floor(difference / (1000 * 3600 * 24));
   };
   const [incidents, setIncidents] = useState([]);
+
+  const handleRemoveIncident = (id) =>{
+    let new_incidents = Array.from(incidents)
+    new_incidents.splice(new_incidents.findIndex(obj => obj.id === id), 1)
+    setIncidents(new_incidents)
+  }
   
+  const handleNewIncident = () => {
+    let data = {
+      "date": '2022-11-04',
+      "impact": "low",
+      "description": "Incident 3"
+    };
+    fetch(`http://${window.location.host.split(":")[0]}:3001/incidents`, {
+      "method": 'POST',
+      "headers": {
+        'Content-Type': 'application/json'
+      },
+      "body": JSON.stringify(data) 
+    }).then(response => response.json())
+    .then(new_incident => {
+      console.log(new_incident);
+      setIncidents([new_incident, ...incidents])
+    })
+  }
+
   useEffect(() => {
-    const port = ""
-    let url = port !== "" ? `http://${window.location.host.split(":")[0]}:${port}/incidents?limit=10` : "/incidents?limit=10";
+    const port = "3001"
+    let url = port !== "" ? `http://${window.location.host.split(":")[0]}:${port}/incidents?_sort=id&_order=desc` : "/incidents?limit=10";
     fetch(url)
     .then(response => response.json())
     .then(incident_list => {
@@ -28,22 +54,8 @@ function App() {
     <div className="App">
       <h1>Last incident</h1>
       <h2>Days w/o incidents: { incidents.length > 0? getDaysCount(incidents[0].date) : 0 }</h2>
-      <div className='incident_list'>
-        <ul>
-          { 
-            incidents.length > 0 ?  
-            incidents.map(incident =>{
-              return <li key={incident.date} className={incident.impact }>
-                <p>{ `${new Date(incident.date).toDateString()} - ${incident.description}` }</p>
-                <span>{ incident.impact }</span>
-                { new Date(incident.date).getDay() === 5 ? <p>Read only friday!!</p> : "" }
-              </li>
-            })
-            : <p>No incidents reported</p>
-          }
-        </ul>
-        <button onClick={() => {console.log("Should retrieve more incidents...")}}>Load more...</button>
-      </div>
+      <IncidentsList incidents={incidents} handleRemoveIncident = { handleRemoveIncident }></IncidentsList>
+      <button onClick={ handleNewIncident}>Add new incident</button>
     </div>
   );
 }
---
+++ src/components/incident_list.js
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react'
+
+const IncidentsList = ({incidents, handleRemoveIncident}) => {
+    const handleDelete = (id) => {
+        fetch(`http://${window.location.host.split(":")[0]}:3001/incidents/${id}`, {
+        "method": 'DELETE',
+        "headers": {
+            'Content-Type': 'application/json'
+        }
+        }).then(response => {
+            console.log(response)
+            if(response.status === 200){
+                handleRemoveIncident(id)
+            }
+            return response.json()
+        })
+    }
+
+    return(
+        <div className='incident_list'>
+        <ul>
+          { 
+            incidents.length > 0 ?  
+            incidents.map(incident =>{
+              return <li key={incident.id} className={incident.impact }>
+                <p>{ `${new Date(incident.date).toDateString()} - ${incident.description}` }</p>
+                <span>{ incident.impact }</span>
+                { new Date(incident.date).getDay() === 5 ? <p>Read only friday!!</p> : "" }
+                <button onClick={ () => handleDelete(incident.id)}>Delete</button>
+              </li>
+            })
+            : <p>No incidents reported</p>
+          }
+        </ul>
+        <button onClick={() => {console.log("Should retrieve more incidents...")}}>Load more...</button>
+      </div>
+    )
+}
+
+export default IncidentsList;
