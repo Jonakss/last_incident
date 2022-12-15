e1b0506e28bf5c0484b5d5d6a1dfd24ab0a200f4
Jonathan Correa Paiva - BQN <jonathan.correa@bqn.com.uy>
2022-12-15 14:18:12 -0300

    update: Change port

--- src/services/incidents.js
+++ src/services/incidents.js
@@ -1,4 +1,4 @@
-const port = "3000"
+const port = "9300"
 
 export const get_all_incidents = (callback) => {
     let url = `http://${window.location.host.split(":")[0]}:${port ? port: window.location.host.split(":")[1]}/incidents?_sort=id&_order=desc&limit=10`;
